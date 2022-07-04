5957d375b4bfaa226b19b6b162207ddbbc6473dc
akilvarli <akilvarli@gmail.com>
2022-07-04 17:31:50 +0300

    first-commit

---
+++ src/lib/auth.js
@@ -0,0 +1,14 @@
+import netlifyIdentity from 'netlify-identity-widget';
+
+export function init(callback) {
+    netlifyIdentity.on('init', user => {
+        callback(user);
+    });
+
+    netlifyIdentity.init();
+}
+
+
+export function logIn(callback) {
+    netlifyIdentity.open()
+}
